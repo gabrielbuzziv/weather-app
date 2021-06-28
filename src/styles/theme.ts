a940691123b483683294e7442c0287baa60fd0d0
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2021-06-28 12:30:44 -0300

    add theme global styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  colors: {
+    heading: '#283953',
+    text: '#7B8DA7',
+
+    blue60: '#618DFF',
+    blue40: '#79A2FF',
+    blue20: '#A8CCFF',
+    blue10: '#EBF3FF',
+
+    white: '#fff'
+  },
+  fonts: {
+    title: 'Inter_700Bold',
+    subtitle: 'Inter_500Medium',
+    text: 'Inter_400Regular'
+  }
+}
