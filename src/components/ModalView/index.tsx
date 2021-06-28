89be54bfae96dc4d7375d5127719e03801cf8369
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2021-06-28 14:17:17 -0300

    add modal view component

---
+++ src/components/ModalView/index.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from 'react';
+import { Modal, ModalProps, TouchableWithoutFeedback } from 'react-native';
+
+import { Overlay, Container, Bar } from './styles';
+
+interface Props extends ModalProps {
+  children: ReactNode;
+  closeModal: () => void;
+}
+
+export function ModalView({ children, closeModal, ...rest }: Props) {
+  return (
+    <Modal transparent statusBarTranslucent animationType="slide" {...rest}>
+      <TouchableWithoutFeedback onPress={closeModal}>
+        <Overlay>
+          <Container>
+            <Bar />
+            {children}
+          </Container>
+        </Overlay>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+}
---
+++ src/components/ModalView/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+import { theme } from '../../styles/theme';
+
+export const Overlay = styled.View`
+  background-color: rgba(0, 0, 0, 0.8);
+  flex: 1;
+  justify-content: flex-end;
+`;
+
+export const Container = styled.View`
+  background-color: ${theme.colors.white};
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  padding: 16px;
+`;
+
+export const Bar = styled.View`
+  background-color: ${theme.colors.blue20};
+  border-radius: 2px;
+  height: 4px;
+  width: 40px;
+  align-self: center;
+  margin-bottom: 24px;
+`
